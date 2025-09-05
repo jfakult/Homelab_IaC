a2c83c5ddca9b3a3954c55a138363f5c09ab3220
root <root@butlah.fakult.net>
2025-09-05 11:32:15 -0400

    bastion up. SSO and Media started

--- gen_etc_hosts.sh
+++ gen_etc_hosts.sh
@@ -4,10 +4,24 @@
 
 INVENTORY="$IAC/ansible/inventory/hosts.ini"
 
+echo "! Generating short hostnames"
 ansible-inventory -J -i "$INVENTORY" --list \
   | jq -r '
       ._meta.hostvars
       | to_entries[]
       | select(.value.ansible_host != null)
       | "\(.value.ansible_host)\t\(.key)"
-    '
+    ' | sort -n
+
+# Also add entries for [name].fakult.net
+echo "! Generating FQDN hostnames"
+ansible-inventory -J -i "$INVENTORY" --list \
+  | jq -r '
+      ._meta.hostvars
+      | to_entries[]
+      | select(.value.ansible_host != null)
+      | "\(.value.ansible_host)\t\(.key).fakult.net"
+    ' | sort -n
+
+
+echo "Paste this into your /etc/hosts (not including the ! printed lines)"
